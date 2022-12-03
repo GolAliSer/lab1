d11bbfd697c930a6a7512fa1b1e56a880f410292
Alina <asgolubeva_1@edu.hse.ru>
2022-12-03 11:55:53 +0300

    commit

--- lab1_4.java.java
+++ lab1_4.java.java
@@ -19,28 +19,29 @@ public class Main {
         N = sc.nextInt();
         if (N < 0) {
             System.out.print("Точек нет.");
+        } else {
+            int schet = 0;
+            while (N != 0) {
+                System.out.print("Введите координату X: ");
+                while (!sc.hasNextInt()) {
+                    System.out.print("Это не целове число. Попробуйте снова: ");
+                    sc.next();
+                }
+                int X = sc.nextInt();
+                System.out.print("Введите координату Y: ");
+                while (!sc.hasNextInt()) {
+                    System.out.print("Это не целове число. Попробуйте снова: ");
+                    sc.next();
+                }
+                int Y = sc.nextInt();
+                if ((X * X) + (Y * Y) <= (R * R)) {
+                    schet = schet + 1;
+                } else {
+                    schet = schet;
+                }
+                N = N - 1;
+            }
+            System.out.print(schet + " точек попали в окружность.");
         }
-        int schet = 0;
-        while (N != 0) {
-            System.out.print("Введите координату X: ");
-            while (!sc.hasNextInt()) {
-                System.out.print("Это не целове число. Попробуйте снова: ");
-                sc.next();
-            }
-            int X = sc.nextInt();
-            System.out.print("Введите координату Y: ");
-            while (!sc.hasNextInt()) {
-                System.out.print("Это не целове число. Попробуйте снова: ");
-                sc.next();
-            }
-            int Y = sc.nextInt();
-            if ((X * X) + (Y * Y) <= (R * R)) {
-                schet = schet + 1;
-            } else {
-                schet = schet;
-            }
-            N = N - 1;
-        }
-        System.out.print(schet + " точек попали в окружность.");
     }
 }
